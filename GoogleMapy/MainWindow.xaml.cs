c0310af353e76446a55844a20d7545d5b1329ee4
Wojciech Janowski <wojtek.janowski@yahoo.pl>
2017-07-17 19:22:39 +0200

    adding basic google maps features

--- GoogleMapy/MainWindow.xaml.cs
+++ GoogleMapy/MainWindow.xaml.cs
@@ -12,17 +12,62 @@ using System.Windows.Media;
 using System.Windows.Media.Imaging;
 using System.Windows.Navigation;
 using System.Windows.Shapes;
+using System.Device.Location;
 
-namespace GoogleMapy
-{
+namespace GoogleMapy {
     /// <summary>
     /// Interaction logic for MainWindow.xaml
     /// </summary>
-    public partial class MainWindow : Window
-    {
-        public MainWindow()
-        {
+    public partial class MainWindow : Window {
+
+        private GeoCoordinate coord;
+        private int zoomConst = 18;
+        public MainWindow() {
             InitializeComponent();
+
+            GeoCoordinateWatcher watcher = new GeoCoordinateWatcher();
+            watcher.TryStart(false, TimeSpan.FromMilliseconds(1000));
+            coord = watcher.Position.Location;
+           
         }
+
+        //load google maps html file
+        private void GridLoaded(object sender, RoutedEventArgs e) {
+            var sURL = AppDomain.CurrentDomain.BaseDirectory + @"html\main_map.html";
+            Uri uri = new Uri(sURL);
+            webBrowser.Navigate(uri);
+           
+        }
+        //set the init properties
+        private void WebBrowser_OnLoadCompleted(object sender, NavigationEventArgs e){
+            if (coord.IsUnknown != true) {
+                webBrowser.InvokeScript("initMap", coord.Latitude, coord.Longitude, zoomConst);
+            }
+            else {
+                webBrowser.InvokeScript("initMap", -25.363, 131.044, zoomConst);
+            }
+        }
+        //login method
+        private void loginButton_Click(object sender, RoutedEventArgs e) {
+            loginLabel.Content = "Zalogowano jako:\n" + loginBox.Text;
+            loginBox.Visibility = Visibility.Hidden;
+            passwordBox.Visibility = Visibility.Hidden;
+            registerButton.Visibility = Visibility.Hidden;
+            loginButton.Visibility = Visibility.Hidden;
+
+            logoutButton.Visibility = Visibility.Visible;
+        }
+
+        //logout method
+        private void logoutButton_Click(object sender, RoutedEventArgs e) {
+            loginLabel.Content = "Logowanie: ";
+            loginBox.Visibility = Visibility.Visible;
+            passwordBox.Visibility = Visibility.Visible;
+            registerButton.Visibility = Visibility.Visible;
+            loginButton.Visibility = Visibility.Visible;
+
+            logoutButton.Visibility = Visibility.Hidden;
+        }
+
     }
 }
